1c99535c8bcb9b9f64f8276db35bf033c27e52e9
maurolonmartin <maurolonmartin@hotmail.com>
2019-11-19 09:19:00 -0500

    Seccion 11 clase 107

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -36,6 +36,7 @@ namespace DatingApp.API
                 Newtonsoft.Json.ReferenceLoopHandling.Ignore;
             });
             services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudynarySettings"));
             services.AddAutoMapper(typeof(DatingRepository).Assembly);
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddScoped<IDatingRepository, DatingRepository>();
